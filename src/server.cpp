b47416cf622a88bc38fa8b32e3c607481d99c838
yly <aptx4869511@126.com>
2026-04-24 11:09:58 +0800

    进入epoll阶段

--- src/server.cpp
+++ src/server.cpp
@@ -1,95 +1,72 @@
 #include <iostream>
-#include<string>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<unistd.h>
-#include<cstring>
-#include<string>
-#include<vector>
+#include <vector>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <cstring>
+#include <sys/epoll.h>
+
 using namespace std;
 
-int main(){
-    //1.创建socket套接字
-    int sockfd = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);//AF_INET:IPv4协议族，SOCK_STREAM:面向连接的TCP协议，IPPROTO_TCP:TCP协议
-    if(sockfd<0)
-    {
-        printf("socket error:errno=%d errmsg=%s\n",errno,strerror(errno));
-        return -1;
-    }
-    else
-    {
-        printf("socket success\n");
-    }
-    //2.绑定socket
-    string ip = "127.0.0.1";
-    int port = 8080;
+int main() {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    struct sockaddr_in sockaddr;//定义一个sockaddr_in结构体变量，用于存储服务器的IP地址和端口号
-    memset(&sockaddr,0,sizeof(sockaddr));
-    sockaddr.sin_family = AF_INET;//设置地址族为AF_INET，表示使用IPv4协议
-    sockaddr.sin_addr.s_addr = inet_addr(ip.c_str());//将字符串形式的IP地址转换为网络字节序的二进制形式，并赋值给sockaddr.sin_addr.s_addr
-    sockaddr.sin_port = htons(port);//将端口号转换为网络字节序，并赋值给sockaddr.sin_port
-    if(bind(sockfd,(struct sockaddr *)&sockaddr,sizeof(sockaddr))<0)
-    {
-        printf("socket bind error:errno=%d errmsg=%s\n",errno,strerror(errno));
-        return -1;  
-    }
-    else
-    {
-        printf("socket bind success: ip=%s port=%d\n",ip.c_str(),port);
-    }
-    //3.监听socket
-    if(listen(sockfd,1024)<0)
-    {
-        printf("socket listen error:errno=%d errmsg=%s\n",errno,strerror(errno));
-        return -1;  
-    }
-    else
-    {
-        printf("socket listening...\n");
-    }
-    //4.接受客户端连接
-vector<int> clients;
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8080);
+    addr.sin_addr.s_addr = INADDR_ANY;
 
-while (true)
-{
-    int connfd = accept(sockfd, nullptr, nullptr);
-    if (connfd >= 0)
-    {
-        printf("new client: %d\n", connfd);
-        clients.push_back(connfd);
-    }
+    bind(sockfd, (sockaddr*)&addr, sizeof(addr));
+    listen(sockfd, 10);
 
+    // 🔥 1. 创建 epoll
+    int epfd = epoll_create(1);
 
-    for (auto it = clients.begin(); it != clients.end(); )
-    {
-        int fd = *it;
-        char buf[1024] = {0};
+    // 2. 把服务器 fd 加进去
+    epoll_event ev{};
+    ev.events = EPOLLIN;
+    ev.data.fd = sockfd;
+    epoll_ctl(epfd, EPOLL_CTL_ADD, sockfd, &ev);
 
-        ssize_t len = recv(fd, buf, sizeof(buf), 0); // 用 fd
+    epoll_event events[1024];
 
-        if (len > 0)
-        {
-            printf("recv client fd=%d msg=%s\n", fd, buf);
-            send(fd, buf, len, 0);
-            ++it;
-        }
-        else if (len == 0)
-        {
-            printf("client closed fd=%d\n", fd);
-            close(fd);
-            it = clients.erase(it);
-        }
-        else
-        {
-            printf("recv error fd=%d errno=%d\n", fd, errno);
-            close(fd);
-            it = clients.erase(it);
+    cout << "epoll server start...\n";
+
+    while (true) {
+        // 🔥 3. 等待事件
+        int n = epoll_wait(epfd, events, 1024, -1);
+
+        for (int i = 0; i < n; i++) {
+            int fd = events[i].data.fd;
+
+            // 👉 有新连接
+            if (fd == sockfd) {
+                int connfd = accept(sockfd, nullptr, nullptr);
+                cout << "new client: " << connfd << endl;
+
+                epoll_event ev_client{};
+                ev_client.events = EPOLLIN;
+                ev_client.data.fd = connfd;
+
+                epoll_ctl(epfd, EPOLL_CTL_ADD, connfd, &ev_client);
+            }
+            else {
+                // 👉 客户端有数据
+                char buf[1024] = {0};
+                int len = recv(fd, buf, sizeof(buf), 0);
+
+                if (len <= 0) {
+                    cout << "client closed: " << fd << endl;
+                    close(fd);
+                    epoll_ctl(epfd, EPOLL_CTL_DEL, fd, nullptr);
+                }
+                else {
+                    cout << "recv fd=" << fd << " msg=" << buf << endl;
+                    send(fd, buf, len, 0);
+                }
+            }
         }
     }
-}
-    //7.关闭连接
+
     close(sockfd);
     return 0;
 }
