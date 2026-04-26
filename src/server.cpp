67de57bb47c572154fe5ea60cd1f8d89a5ea4c6d
yly <aptx4869511@126.com>
2026-04-26 16:13:09 +0800

    设定connection用map接收客户端

--- src/server.cpp
+++ src/server.cpp
@@ -3,17 +3,28 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <cstring>
+#include <string>
 #include <sys/epoll.h>
 #include <fcntl.h>
+#include <map>
 
 using namespace std;
 
+class Connection {//补充数据和状态
+public:
+    int fd;
+    string buffer;//缓存数据
+
+    Connection(int fd_) : fd(fd_) {}
+
+};
+
 void setNonBlocking(int fd){// 设置 fd 为非阻塞模式
-    int flags = fcntl(fd, F_GETFL, 0);
+    int flags = fcntl(fd, F_GETFL, 0);// 获取当前 fd 的 flags
     fcntl(fd, F_SETFL, flags | O_NONBLOCK);
 }
 
-void handleAccept(int sockfd, int epfd){
+void handleAccept(int sockfd, int epfd, map<int, Connection*>& conns){
     while (true) {
         int connfd = accept(sockfd, nullptr, nullptr);
         if (connfd < 0) {
@@ -26,7 +37,8 @@ void handleAccept(int sockfd, int epfd){
         }
         setNonBlocking(connfd);
         cout << "new client: " << connfd << endl;
-
+        // 创建连接对象并保存
+        conns[connfd] = new Connection(connfd);
         epoll_event ev_client{};
         ev_client.events = EPOLLIN;
         ev_client.data.fd = connfd;
@@ -35,19 +47,25 @@ void handleAccept(int sockfd, int epfd){
     }
 }
 
-void handleRead(int fd, int epfd){
+void handleRead(int fd, int epfd,map<int, Connection*>& conns){
+    Connection* conn = conns[fd];// 获取连接对象
+
+    char buf[1024];
     while (true) {
-        char buf[1024];
+        
         int len = recv(fd, buf, sizeof(buf), 0);
 
         if (len > 0) {
-            cout << "recv fd=" << fd << " msg=" << string(buf, len) << endl;
-            send(fd, buf, len, 0);
+            //不直接处理存起来
+            conn->buffer.append(buf, len);
         }
         else if (len == 0) {
             cout << "client closed: " << fd << endl;
-            close(fd);
             epoll_ctl(epfd, EPOLL_CTL_DEL, fd, nullptr);
+            close(fd);
+            
+            delete conn;
+            conns.erase(fd);
             break;
         }
         else {
@@ -56,13 +74,25 @@ void handleRead(int fd, int epfd){
             } else {
                 epoll_ctl(epfd, EPOLL_CTL_DEL, fd, nullptr);
                 close(fd);
+                delete conn;
+                conns.erase(fd);
                 break;
             }
         }
     }
+    //处理buffer中的数据
+    size_t pos;// 处理完整的请求
+    while((pos = conn->buffer.find('\n')) != string::npos){
+        string msg = conn->buffer.substr(0, pos);
+        conn->buffer.erase(0, pos + 1);
+
+        cout<<"完整消息："<<msg<<endl;
+        send(fd, msg.c_str(), msg.size(), 0);// 回显给客户端
+    }
 }
 
 int main() {
+    map<int, Connection*> connections;
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     setNonBlocking(sockfd);// 设置服务器 fd 为非阻塞模式
     sockaddr_in addr{};
@@ -95,10 +125,10 @@ int main() {
 
             // 有新连接
             if (fd == sockfd) {// 服务器 fd 有事件，说明有新连接
-                handleAccept(sockfd, epfd);
+                handleAccept(sockfd, epfd, connections);
             }
             else {// 客户端有事件，说明有数据可读
-                handleRead(fd, epfd);
+                handleRead(fd, epfd, connections);
             }
         }
     }
